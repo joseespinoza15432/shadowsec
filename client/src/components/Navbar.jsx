5fa51b97c5865f8225ba068d524334b414974c0f
joseespinoza15432 <jose.espinoza15432@gmail.com>
2025-04-20 06:01:50 -0700

    typo fix

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,19 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../pages/Home.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../pages/Home.css";
 
 export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-content">
-        <Link to="/" className="navbar-logo">SHADOWSEC</Link>
+        <Link to="/" className="navbar-logo">
+          SHADOWSEC
+        </Link>
         <div className="navbar-links">
-          <Link to="/mission" className="navbar-link">Mission</Link>
-          <Link to="/my-files" className="navbar-link">Files</Link>
-          <Link to="/phishing" className="navbar-link">Phishing</Link>
-          <Link to="/upload" className="navbar-link">Upload</Link>
-          <Link to="/chat" className="navbar-link">Chat</Link>
-          <Link to="/contact" className="navbar-link">Contac Us</Link>
+          <Link to="/mission" className="navbar-link">
+            Mission
+          </Link>
+          <Link to="/phishing" className="navbar-link">
+            Phishing
+          </Link>
+          <Link to="/upload" className="navbar-link">
+            Upload
+          </Link>
+          <Link to="/my-files" className="navbar-link">
+            Files
+          </Link>
+          <Link to="/chat" className="navbar-link">
+            Chat
+          </Link>
+          <Link to="/contact" className="navbar-link">
+            Contact Us
+          </Link>
         </div>
       </div>
     </nav>
